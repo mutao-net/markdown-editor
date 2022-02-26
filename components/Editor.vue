a0f39417dfccd8713c3463c5a74a3a4458bddc40
mutao-net <sasaki.h1026@gmail.com>
2022-02-26 18:17:52 +0900

    add src

---
+++ components/Editor.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="markdown-editor">
+    <no-ssr>
+      <mavon-editor :toolbars="markdownOption" language="ja" v-model="model" />
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: {
+      type: Object | null,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      markdownOption: {
+        bold: true,
+        italic: true,
+        header: true,
+        underline: true,
+        strikethrough: true,
+        mark: true,
+        quote: true,
+        ol: true,
+        ul: true,
+        link: true,
+        imagelink: true,
+        code: true,
+        table: true,
+        fullscreen: true,
+        htmlcode: true,
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.markdown-editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
